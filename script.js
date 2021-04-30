5b91fe13eee83fd06de6be01021390738119a1f3
langloks <clara.langlois@wanadoo.fr>
2021-04-30 13:35:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const randomQuestion = document.getElementById('random-question');
+const answer = document.getElementById('answer');
+let check = document.getElementById('check');
+
+const pronoms = ["'je'", "'tu'", "'il'", "'elle'", "'nous'", "'vous'", "'ils'", "'elles'"];
+const terminaisonEr = ["e", "es", "e", "e", "ons", "ez", "ent","ent"];
+const terminaisonIr = ["is", "is", "it", "it", "issons", "issez", "issent","issent"];
+const terminaisonRe = ["s", "s", "", "", "ons", "ez", "ent","ent"];
+const groupes = ["'first group'", "'second group'", "'third group'"];
+
+function randomIndex (list) {
+   const index = Math.floor(Math.random() * list.length)
+ return  index;
+}
+
+    const randomPronom = randomIndex(pronoms);
+    const choosenPronom = pronoms[randomPronom];
+    const choosenGroupe = groupes[randomIndex(groupes)];
+    randomQuestion.innerText = "conjugate " + choosenPronom + " with " + choosenGroupe;
+    const termEr = terminaisonEr[randomPronom];
+    const termIr = terminaisonIr[randomPronom];
+    const termRe = terminaisonRe[randomPronom];
+
+
+function checkAnswer (){
+    if (choosenGroupe == "'first group'" && answer.value == termEr) {
+        check.innerText = "Bravoooooo";
+            }
+            else if (choosenGroupe == "'second group'" && answer.value == termIr) {
+                check.innerText = "Bravoooooo";
+                    }
+                    else if (choosenGroupe == "'third group'" && answer.value == termRe ) {
+                        check.innerText = "Bravoooooo";
+                            }
+                            else {
+                                check.innerText = "Nope";
+                            }
+}
